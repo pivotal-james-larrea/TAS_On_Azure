679a9fad0f49786fe4ace92a3c12b5b41ba00ac6
pivotal-james-larrea <jlarrea@pivotal.io>
2020-10-15 22:23:43 -0400

    modified how sp identifier is defined and added lb creation

--- tas_automation.sh
+++ tas_automation.sh
@@ -14,20 +14,18 @@ echo "Log in with your Pivotal AD account on your browser"
 az login
 
 read -p 'Which support team are you a part of? (useast or uswest): ' GSS_TEAM
+read -p 'Please enter a unique name for your resource group - all lowercase (Example: jsmith): ' RESOURCE_GROUP
+read -sp 'Please enter a new password for Service Principal for Bosh: ' SP_SECRET
+
 # You should only have one GSS subscription but just in case
 SUBSCRIPTION_ID=$(az account list | grep -i $GSS_TEAM -B 3 | grep id | cut -b 12-47)
+TENANT_ID=$(az account list | grep -i $GSS_TEAM -A 2 | grep tenantId | cut -b 18-53)
+SP_NAME="http://BoshAzure$RESOURCE_GROUP"
+
 az account set --subscription $SUBSCRIPTION_ID
-
-TENANT_ID=$(az account list | grep -i "GSS-CE-USEAST-AZURE" -A 2 | grep tenantId | cut -b 18-53)
-
-read -p 'Please enter a new password for Service Principal for Bosh: ' SP_SECRET
-read -p 'Please enter a unique identifier-uris value (Example: http://BoshAzureJSMITH) ' SP_NAME
-read -p 'Please enter a unique name for your resource group - all lowercase (Example: jsmith): ' RESOURCE_GROUP
-
 az ad app create --display-name "Service Principal for BOSH" \
 --password $SP_SECRET --homepage "http://BOSHAzureCPI" \
 --identifier-uris $SP_NAME
-
 
 APPLICATION_ID=$(az ad app list --identifier-uri $SP_NAME | grep appId | cut -b 15-50)
 
@@ -35,7 +33,7 @@ echo "Creating a Service Principal..."
 az ad sp create --id $APPLICATION_ID
 
 echo "Assigning your Service Principal the Owner role..."
-sleep 5
+sleep 10
 az role assignment create --assignee $SP_NAME \
 --role "Owner" --scope /subscriptions/$SUBSCRIPTION_ID
 
@@ -177,3 +175,33 @@ az storage container create --name bosh \
 --connection-string $CONNECTION_STRING3
 az storage container create --name stemcell \
 --connection-string $CONNECTION_STRING3
+
+
+echo "Creating Load Balancers..."
+
+az network lb create --name pcf-lb \
+--resource-group $RESOURCE_GROUP --location $LOCATION \
+--backend-pool-name pcf-lb-be-pool --frontend-ip-name pcf-lb-fe-ip \
+--public-ip-address pcf-lb-ip --public-ip-address-allocation Static \
+--sku Standard
+
+az network lb probe create --lb-name pcf-lb \
+--name http8080 --resource-group $RESOURCE_GROUP \
+--protocol Http --port 8080 --path health
+
+az network lb rule create --lb-name pcf-lb \
+--name http --resource-group $RESOURCE_GROUP \
+--protocol Tcp --frontend-port 80 \
+--backend-port 80 --frontend-ip-name pcf-lb-fe-ip \
+--backend-pool-name pcf-lb-be-pool \
+--probe-name http8080
+
+az network lb rule create --lb-name pcf-lb \
+--name https --resource-group $RESOURCE_GROUP \
+--protocol Tcp --frontend-port 443 \
+--backend-port 443 --frontend-ip-name pcf-lb-fe-ip \
+--backend-pool-name pcf-lb-be-pool \
+--probe-name http8080
+
+az network public-ip show --name pcf-lb-ip --resource-group $RESOURCE_GROUP > azure_dep_out
+
